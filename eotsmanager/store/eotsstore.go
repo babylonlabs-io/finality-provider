96d70b6ed2db1ea4459f223f8cda4df8091fb280
Lazar <lukic.lazar@yahoo.com>
2024-12-03 15:09:55 +0100

    move db

--- eotsmanager/store/eotsstore.go
+++ eotsmanager/store/eotsstore.go
@@ -1,7 +1,11 @@
 package store
 
 import (
+	"encoding/binary"
 	"fmt"
+	"github.com/babylonlabs-io/finality-provider/eotsmanager/proto"
+	pm "google.golang.org/protobuf/proto"
+	"time"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
@@ -10,7 +14,8 @@ import (
 )
 
 var (
-	eotsBucketName = []byte("fpKeyNames")
+	eotsBucketName       = []byte("fpKeyNames")
+	signRecordBucketName = []byte("signRecord")
 )
 
 type EOTSStore struct {
@@ -29,6 +34,11 @@ func NewEOTSStore(db kvdb.Backend) (*EOTSStore, error) {
 func (s *EOTSStore) initBuckets() error {
 	return kvdb.Batch(s.db, func(tx kvdb.RwTx) error {
 		_, err := tx.CreateTopLevelBucket(eotsBucketName)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.CreateTopLevelBucket(signRecordBucketName)
 		if err != nil {
 			return err
 		}
@@ -92,4 +102,70 @@ func (s *EOTSStore) GetEOTSKeyName(pk []byte) (string, error) {
 	}
 
 	return keyName, nil
+}
+
+func (s *EOTSStore) SaveSignRecord(
+	height uint64,
+	BlockHash []byte,
+	PublicKey []byte,
+	Signature []byte,
+) error {
+	key := uint64ToBytes(height)
+
+	return kvdb.Batch(s.db, func(tx kvdb.RwTx) error {
+		bucket := tx.ReadWriteBucket(signRecordBucketName)
+		if bucket == nil {
+			return ErrCorruptedEOTSDb
+		}
+
+		if bucket.Get(key) != nil {
+			return ErrDuplicateSignRecord
+		}
+
+		signRecord := &proto.SigningRecord{
+			BlockHash: BlockHash,
+			PublicKey: PublicKey,
+			Signature: Signature,
+			Timestamp: time.Now().UnixMilli(),
+		}
+
+		marshalled, err := pm.Marshal(signRecord)
+		if err != nil {
+			return err
+		}
+
+		return bucket.Put(key, marshalled)
+	})
+}
+
+func (s *EOTSStore) GetSignRecord(height uint64) (*proto.SigningRecord, error) {
+	key := uint64ToBytes(height)
+	res := &proto.SigningRecord{}
+
+	err := s.db.View(func(tx kvdb.RTx) error {
+		bucket := tx.ReadBucket(signRecordBucketName)
+		if bucket == nil {
+			return ErrCorruptedEOTSDb
+		}
+
+		signRecordBytes := bucket.Get(key)
+		if signRecordBytes == nil {
+			return ErrSignRecordNotFound
+		}
+
+		return pm.Unmarshal(signRecordBytes, res)
+	}, func() {})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// Converts an uint64 value to a byte slice.
+func uint64ToBytes(v uint64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], v)
+	return buf[:]
 }
--- eotsmanager/store/eotsstore_test.go
+++ eotsmanager/store/eotsstore_test.go
@@ -1,13 +1,14 @@
 package store_test
 
 import (
+	"github.com/babylonlabs-io/babylon/testutil/datagen"
+	"github.com/babylonlabs-io/finality-provider/eotsmanager/proto"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/stretchr/testify/require"
 	"math/rand"
 	"os"
 	"testing"
-
-	"github.com/babylonlabs-io/babylon/testutil/datagen"
-	"github.com/btcsuite/btcd/btcec/v2/schnorr"
-	"github.com/stretchr/testify/require"
+	"time"
 
 	"github.com/babylonlabs-io/finality-provider/eotsmanager/config"
 	"github.com/babylonlabs-io/finality-provider/eotsmanager/store"
@@ -61,5 +62,64 @@ func FuzzEOTSStore(f *testing.F) {
 		require.NoError(t, err)
 		_, err = vs.GetEOTSKeyName(schnorr.SerializePubKey(randomBtcPk))
 		require.ErrorIs(t, err, store.ErrEOTSKeyNameNotFound)
+	})
+}
+
+// FuzzSignStore tests save sing records
+func FuzzSignStore(f *testing.F) {
+	testutil.AddRandomSeedsToFuzzer(f, 10)
+	f.Fuzz(func(t *testing.T, seed int64) {
+		r := rand.New(rand.NewSource(seed))
+
+		homePath := t.TempDir()
+		cfg := config.DefaultDBConfigWithHomePath(homePath)
+
+		dbBackend, err := cfg.GetDBBackend()
+		require.NoError(t, err)
+
+		vs, err := store.NewEOTSStore(dbBackend)
+		require.NoError(t, err)
+
+		defer func() {
+			dbBackend.Close()
+			err := os.RemoveAll(homePath)
+			require.NoError(t, err)
+		}()
+
+		expectedRecord := proto.SigningRecord{
+			BlockHash: testutil.GenRandomByteArray(r, 32),
+			PublicKey: testutil.GenRandomByteArray(r, 33),
+			Signature: testutil.GenRandomByteArray(r, 32),
+			Timestamp: time.Now().UnixMilli(),
+		}
+		expectedHeight := r.Uint64()
+
+		// save for the first time
+		err = vs.SaveSignRecord(
+			expectedHeight,
+			expectedRecord.BlockHash,
+			expectedRecord.PublicKey,
+			expectedRecord.Signature,
+		)
+		require.NoError(t, err)
+
+		// try to save the record at the same height
+		err = vs.SaveSignRecord(
+			expectedHeight,
+			expectedRecord.BlockHash,
+			expectedRecord.PublicKey,
+			expectedRecord.Signature,
+		)
+		require.ErrorIs(t, err, store.ErrDuplicateSignRecord)
+
+		signRecordFromDB, err := vs.GetSignRecord(expectedHeight)
+		require.NoError(t, err)
+		require.Equal(t, expectedRecord.PublicKey, signRecordFromDB.PublicKey)
+		require.Equal(t, expectedRecord.BlockHash, signRecordFromDB.BlockHash)
+		require.Equal(t, expectedRecord.Signature, signRecordFromDB.Signature)
+
+		rndHeight := r.Uint64()
+		_, err = vs.GetSignRecord(rndHeight)
+		require.ErrorIs(t, err, store.ErrSignRecordNotFound)
 	})
 }
