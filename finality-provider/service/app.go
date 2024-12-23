7669020fe97f33b51dac03683559c66c006354ab
Lazar <lukic.lazar@yahoo.com>
2024-12-23 15:30:11 +0100

    start fp after register

--- finality-provider/service/app.go
+++ finality-provider/service/app.go
@@ -413,6 +413,14 @@ func (app *FinalityProviderApp) CreateFinalityProvider(
 			return nil, err
 		}
 
+		if err = app.startFinalityProviderInstance(storedFp.GetBIP340BTCPK(), ""); err != nil {
+			app.logger.Error(
+				"failed to start fp instance",
+				zap.String("eots_pk", pkHex),
+				zap.Error(err),
+			)
+		}
+
 		return &CreateFinalityProviderResult{
 			FpInfo: storedFp.ToFinalityProviderInfo(),
 			TxHash: successResponse.txHash,
