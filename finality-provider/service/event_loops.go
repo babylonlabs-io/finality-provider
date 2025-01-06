200c83ea8578f1795aeccd933e4af6a41c5f85f1
Cirrus Gai <greferry@gmail.com>
2025-01-06 21:54:15 +0800

    chore: Allow running of jailed fp (#260)
    Closes #259. This allows jailed fps to still be able to send randomness
    and it could start voting immdiately after unjailing.

--- finality-provider/service/event_loops.go
+++ finality-provider/service/event_loops.go
@@ -65,12 +65,6 @@ func (app *FinalityProviderApp) monitorCriticalErr() {
 					zap.String("pk", criticalErr.fpBtcPk.MarshalHex()))
 				continue
 			}
-			if errors.Is(criticalErr.err, ErrFinalityProviderJailed) {
-				app.setFinalityProviderJailed(fpi)
-				app.logger.Debug("the finality-provider has been jailed",
-					zap.String("pk", criticalErr.fpBtcPk.MarshalHex()))
-				continue
-			}
 			app.logger.Fatal(instanceTerminatingMsg,
 				zap.String("pk", criticalErr.fpBtcPk.MarshalHex()), zap.Error(criticalErr.err))
 		case <-app.quit:
@@ -176,6 +170,11 @@ func (app *FinalityProviderApp) unjailFpLoop() {
 				zap.String("btc_pk", req.btcPubKey.MarshalHex()),
 				zap.String("txHash", res.TxHash),
 			)
+
+			// set the status to INACTIVE by default
+			// the status will be changed to ACTIVE
+			// if it has voting power for the next height
+			app.fps.MustSetFpStatus(req.btcPubKey.MustToBTCPK(), proto.FinalityProviderStatus_INACTIVE)
 
 			req.successResponse <- &UnjailFinalityProviderResponse{
 				TxHash: res.TxHash,
