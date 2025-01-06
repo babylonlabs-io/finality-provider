e451da067bd998c60341a0285f5c612d005b3e9a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-01-06 11:27:08 -0300

    chore: address pr comments

--- eotsmanager/cmd/eotsd/daemon/pop.go
+++ eotsmanager/cmd/eotsd/daemon/pop.go
@@ -53,13 +53,13 @@ type PoPExport struct {
 
 func NewExportPopCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "pop-export",
-		Short: "Exports the Proof of Possession by signing over the BABY address with the EOTS private key and the EOTS public key with the BABY private key.",
-		Long: `Parse the address from the BABY keyring, loads the address hash it with
+		Use:   "export-pop",
+		Short: "Exports the Proof of Possession by (1) signing over the BABY address with the EOTS private key and (2) signing over the EOTS public key with the BABY private key.",
+		Long: `Parse the address from the BABY keyring, load the address, hash it with
 		sha256 and sign based on the EOTS key associated with the key-name or eots-pk flag.
 		If the both flags are supplied, eots-pk takes priority. Use the generated signature
 		to build a Proof of Possession. For the creation of the BABY signature over the eots pk,
-		it loads the BABY key pair and signs the eots-pk hex with adr036 and exports it.`,
+		it loads the BABY key pair and signs the eots-pk hex and exports it.`,
 		RunE: exportPop,
 	}
 
