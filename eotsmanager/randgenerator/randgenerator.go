4b49eec3604169de13239c06701ad90139e6be1a
Fangyu Gai <greferry@gmail.com>
2025-02-10 17:43:31 +0800

    fix random generation bias

--- eotsmanager/randgenerator/randgenerator.go
+++ eotsmanager/randgenerator/randgenerator.go
@@ -13,14 +13,26 @@ import (
 // GenerateRandomness generates a random scalar with the given key and src
 // the result is deterministic with each given input
 func GenerateRandomness(key []byte, chainID []byte, height uint64) (*eots.PrivateRand, *eots.PublicRand) {
-	// calculate the randomn hash of the key concatenated with chainID and height
-	digest := hmac.New(sha256.New, key)
-	digest.Write(append(sdk.Uint64ToBigEndian(height), chainID...))
-	randPre := digest.Sum(nil)
-
-	// convert the hash into private random
 	var randScalar btcec.ModNScalar
-	randScalar.SetByteSlice(randPre)
+
+	// add iteration as part of HMAC generation to get a uniformly random
+	// value for randScalar via rejection sampling
+	iteration := uint64(0)
+	for {
+		// calculate the random hash with iteration count
+		digest := hmac.New(sha256.New, key)
+		digest.Write(append(append(sdk.Uint64ToBigEndian(height), chainID...), sdk.Uint64ToBigEndian(iteration)...))
+		randPre := digest.Sum(nil)
+
+		// increase iteration count and sample again until overflow does not
+		// happen. It is fine as the chance of overflow is very small (2^-128)
+		if overflow := randScalar.SetByteSlice(randPre); !overflow {
+			break
+		}
+		iteration++
+	}
+
+	// convert the valid scalar into private random
 	privRand := secp256k1.NewPrivateKey(&randScalar)
 	var j secp256k1.JacobianPoint
 	privRand.PubKey().AsJacobian(&j)
