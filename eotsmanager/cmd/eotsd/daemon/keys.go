09ce71bc5ebaabccc7311401d6cc5a1439b5f110
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-02-17 12:54:58 -0300

    chore: remove unecessary flag (#341)

--- eotsmanager/cmd/eotsd/daemon/keys.go
+++ eotsmanager/cmd/eotsd/daemon/keys.go
@@ -44,7 +44,6 @@ func NewKeysCmd() *cobra.Command {
 	}
 
 	listCmd.RunE = runCommandPrintAllKeys
-	listCmd.Flags().StringP(flags.FlagOutput, "o", flags.OutputFormatText, "Output format (text|json)")
 
 	if showCmd := util.GetSubCommand(keysCmd, "show"); showCmd != nil {
 		showCmd.RunE = func(cmd *cobra.Command, args []string) error {
