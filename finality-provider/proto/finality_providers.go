ab6a91ac977c06a3dd1cf26ea159e0946e1ee0df
tom <tomasguerraalda@hotmail.com>
2025-02-26 16:00:58 -0300

    add types funcs

--- finality-provider/proto/finality_providers.go
+++ finality-provider/proto/finality_providers.go
@@ -1,6 +1,7 @@
 package proto
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -8,6 +9,7 @@ import (
 
 	"cosmossdk.io/math"
 	bbn "github.com/babylonlabs-io/babylon/types"
+	btcstktypes "github.com/babylonlabs-io/babylon/x/btcstaking/types"
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -55,4 +57,33 @@ func NewCommissionInfoWithTime(maxRate, maxChangeRate math.LegacyDec, updatedAt 
 		MaxChangeRate: maxChangeRate.String(),
 		UpdateTime:    timestamppb.New(updatedAt),
 	}
+}
+
+func NewCommissionRates(rate, maxRate, maxChangeRate math.LegacyDec) *CommissionRates {
+	return &CommissionRates{
+		Rate:          rate.String(),
+		MaxRate:       maxRate.String(),
+		MaxChangeRate: maxChangeRate.String(),
+	}
+}
+
+func (req *CreateFinalityProviderRequest) GetCommissionRates() (rates btcstktypes.CommissionRates, err error) {
+	if req.Commission == nil {
+		return rates, errors.New("nil Commission in request. Cannot get CommissionRates")
+	}
+
+	rate, err := math.LegacyNewDecFromStr(req.Commission.Rate)
+	if err != nil {
+		return rates, fmt.Errorf("invalid commission rate: %w", err)
+	}
+	maxRate, err := math.LegacyNewDecFromStr(req.Commission.MaxRate)
+	if err != nil {
+		return rates, fmt.Errorf("invalid commission max rate: %w", err)
+	}
+	maxChangeRate, err := math.LegacyNewDecFromStr(req.Commission.MaxChangeRate)
+	if err != nil {
+		return rates, fmt.Errorf("invalid commission max change rate: %w", err)
+	}
+
+	return btcstktypes.NewCommissionRates(rate, maxRate, maxChangeRate), nil
 }
