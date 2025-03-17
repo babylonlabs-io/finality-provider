44270fe3916b70a3f0f116806d3bf2845cf152b8
Sam Ricotta <samanthalricotta@gmail.com>
2025-03-17 17:14:03 +0200

    Update config.go

--- eotsmanager/config/config.go
+++ eotsmanager/config/config.go
@@ -89,6 +89,8 @@ func (cfg *Config) Validate() error {
 
 	if cfg.KeyringBackend == "" {
 		return fmt.Errorf("the keyring backend should not be empty")
+	} else if cfg.KeyringBackend != "test" {
+		return fmt.Errorf("the keyring backend should be test")
 	}
 
 	if cfg.Metrics == nil {
