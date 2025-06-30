93f629b3ae194cec6937ad251889a2c3b89939cb
Lazar <lukic.lazar@yahoo.com>
2025-06-30 22:02:04 +0900

    lint fix

--- finality-provider/signingcontext/builder.go
+++ finality-provider/signingcontext/builder.go
@@ -20,31 +20,32 @@ var (
 	AccFinality = authtypes.NewModuleAddress(finalitytypes.ModuleName)
 )
 
-func btcStakingV0Context(operationTag string, chainId string, address string) string {
-	return fmt.Sprintf("%s/%s/%s/%s/%s", protocolName, versionV0, operationTag, chainId, address)
+func btcStakingV0Context(operationTag string, chainID string, address string) string {
+	return fmt.Sprintf("%s/%s/%s/%s/%s", protocolName, versionV0, operationTag, chainID, address)
 }
 
 // HashedHexContext returns the hex encoded sha256 hash of the context string i.e
 // hex(sha256(contextString))
 func HashedHexContext(contextString string) string {
 	bytes := sha256.Sum256([]byte(contextString))
+
 	return hex.EncodeToString(bytes[:])
 }
 
 // FpPopContextV0 returns context string in format:
-// btcstaking/0/fp_pop/{chainId}/{address}
-func FpPopContextV0(chainId string, address string) string {
-	return HashedHexContext(btcStakingV0Context(fpPop, chainId, address))
+// btcstaking/0/fp_pop/{chainID}/{address}
+func FpPopContextV0(chainID string, address string) string {
+	return HashedHexContext(btcStakingV0Context(fpPop, chainID, address))
 }
 
 // FpRandCommitContextV0 returns context string in format:
-// btcstaking/0/fp_rand_commit/{chainId}/{address}
-func FpRandCommitContextV0(chainId string, address string) string {
-	return HashedHexContext(btcStakingV0Context(fpRandCommit, chainId, address))
+// btcstaking/0/fp_rand_commit/{chainID}/{address}
+func FpRandCommitContextV0(chainID string, address string) string {
+	return HashedHexContext(btcStakingV0Context(fpRandCommit, chainID, address))
 }
 
 // FpFinVoteContextV0 returns context string in format:
-// btcstaking/0/fp_fin_vote/{chainId}/{address}
-func FpFinVoteContextV0(chainId string, address string) string {
-	return HashedHexContext(btcStakingV0Context(fpFinVote, chainId, address))
+// btcstaking/0/fp_fin_vote/{chainID}/{address}
+func FpFinVoteContextV0(chainID string, address string) string {
+	return HashedHexContext(btcStakingV0Context(fpFinVote, chainID, address))
 }
