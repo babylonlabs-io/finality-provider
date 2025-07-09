6b4a422b9610d4ca9ead6965900ec3c9b489f3bc
Lazar <lukic.lazar@yahoo.com>
2025-07-09 16:24:41 +0200

    wip interface refactor

--- clientcontroller/api/interface.go
+++ clientcontroller/api/interface.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"cosmossdk.io/math"
 	btcstakingtypes "github.com/babylonlabs-io/babylon/x/btcstaking/types"
 	"github.com/btcsuite/btcd/btcec/v2"
@@ -97,4 +98,95 @@ type ConsumerController interface {
 	QueryFinalityActivationBlockHeight() (uint64, error)
 
 	Close() error
+}
+
+type ConsumerControllerExplicit[T types.BlockDescription] interface {
+	RandomnessCommitter
+	BlockQuerier[T]
+	FinalityOperator
+
+	Close() error
+}
+
+// RandomnessCommitter handles public randomness commitment operations
+type RandomnessCommitter interface {
+	// CommitPubRandList commits a list of EOTS public randomness to the consumer chain
+	CommitPubRandList(ctx context.Context, req *CommitPubRandListRequest) (*types.TxResponse, error)
+
+	// QueryLastPublicRandCommit returns the last public randomness commitment
+	QueryLastPublicRandCommit(ctx context.Context, fpPk *btcec.PublicKey) (*types.PubRandCommit, error)
+}
+
+type BlockQuerier[T types.BlockDescription] interface {
+	// QueryLatestFinalizedBlock returns the latest finalized block
+	QueryLatestFinalizedBlock(ctx context.Context) (T, error)
+
+	// QueryBlock queries the block at the given height
+	QueryBlock(ctx context.Context, height uint64) (T, error)
+
+	// QueryIsBlockFinalized queries if the block at the given height is
+	// finalized
+	QueryIsBlockFinalized(ctx context.Context, height uint64) (bool, error)
+
+	// QueryBlocks returns a list of blocks from startHeight to endHeight
+	QueryBlocks(ctx context.Context, req *QueryBlocksRequest) ([]T, error)
+
+	// QueryLatestBlockHeight queries the tip block height of the consumer chain
+	QueryLatestBlockHeight(ctx context.Context) (uint64, error)
+
+	// QueryActivatedHeight returns the activated height of the consumer chain
+	QueryActivatedHeight(ctx context.Context) (uint64, error)
+
+	// QueryFinalityActivationBlockHeight return the block height when finality voting starts
+	QueryFinalityActivationBlockHeight(ctx context.Context) (uint64, error)
+}
+
+// FinalityOperator handles finality signature submission operations
+type FinalityOperator interface {
+	// SubmitBatchFinalitySigs submits a batch of finality signatures to the consumer chain
+	SubmitBatchFinalitySigs(ctx context.Context, req *SubmitBatchFinalitySigsRequest) (*types.TxResponse, error)
+
+	// UnjailFinalityProvider sends an unjail transaction to the consumer chain
+	UnjailFinalityProvider(ctx context.Context, fpPk *btcec.PublicKey) (*types.TxResponse, error)
+
+	// QueryFinalityProviderHasPower queries whether the finality provider has voting power at a given height
+	QueryFinalityProviderHasPower(ctx context.Context, req *QueryFinalityProviderHasPowerRequest) (bool, error)
+
+	// QueryFinalityProviderStatus queries the finality provider status
+	QueryFinalityProviderStatus(ctx context.Context, fpPk *btcec.PublicKey) (*FinalityProviderStatusResponse, error)
+
+	// QueryFinalityProviderHighestVotedHeight queries the highest voted height of the given finality provider
+	QueryFinalityProviderHighestVotedHeight(ctx context.Context, fpPk *btcec.PublicKey) (uint64, error)
+}
+
+type SubmitBatchFinalitySigsRequest struct {
+	FpPk        *btcec.PublicKey
+	Blocks      []*types.BlockInfo
+	PubRandList []*btcec.FieldVal
+	ProofList   [][]byte
+	Sigs        []*btcec.ModNScalar
+}
+
+type QueryBlocksRequest struct {
+	StartHeight uint64
+	EndHeight   uint64
+	Limit       uint32
+}
+
+type CommitPubRandListRequest struct {
+	FpPk        *btcec.PublicKey
+	StartHeight uint64
+	NumPubRand  uint64
+	Commitment  []byte
+	Sig         *schnorr.Signature
+}
+
+type FinalityProviderStatusResponse struct {
+	Slashed bool
+	Jailed  bool
+}
+
+type QueryFinalityProviderHasPowerRequest struct {
+	FpPk        *btcec.PublicKey
+	BlockHeight uint64
 }
