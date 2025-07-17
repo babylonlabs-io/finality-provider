04c0eccaf4321ac88493a49a66017cbe7c0b7862
Lazar <lukic.lazar@yahoo.com>
2025-07-17 10:40:01 +0200

    lint fix

--- finality-provider/cmd/fpd/daemon/commit_pr.go
+++ finality-provider/cmd/fpd/daemon/commit_pr.go
@@ -66,7 +66,7 @@ func runCommandCommitPubRand(ctx client.Context, cmd *cobra.Command, args []stri
 	return RunCommandCommitPubRandWithConfig(ctx, cmd, homePath, cfg, args)
 }
 
-func RunCommandCommitPubRandWithConfig(ctx client.Context, cmd *cobra.Command, homePath string, cfg *fpcfg.Config, args []string) error {
+func RunCommandCommitPubRandWithConfig(_ client.Context, cmd *cobra.Command, homePath string, cfg *fpcfg.Config, args []string) error {
 	fpPk, err := bbntypes.NewBIP340PubKeyFromHex(args[0])
 	if err != nil {
 		return err
