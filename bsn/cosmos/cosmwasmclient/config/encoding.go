5f85b5e5e6ed638040520f11e31e216308301535
Lazar <lukic.lazar@yahoo.com>
2025-08-07 21:00:44 +0200

    housekeeping

--- bsn/cosmos/cosmwasmclient/config/encoding.go
+++ bsn/cosmos/cosmwasmclient/config/encoding.go
@@ -8,25 +8,11 @@ import (
 	wasmdparams "github.com/CosmWasm/wasmd/app/params"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	dbm "github.com/cosmos/cosmos-db"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 )
 
 // GetWasmdEncodingConfig creates a temporary WasmApp and returns its EncodingConfig.
 func GetWasmdEncodingConfig() wasmdparams.EncodingConfig {
-	return GetWasmdEncodingConfigWithPrefix("")
-}
-
-// GetWasmdEncodingConfigWithPrefix creates a temporary WasmApp with the specified account prefix and returns its EncodingConfig.
-func GetWasmdEncodingConfigWithPrefix(accountPrefix string) wasmdparams.EncodingConfig {
-	// Set account prefix if provided before creating the app
-	if accountPrefix != "" {
-		config := sdk.GetConfig()
-		config.SetBech32PrefixForAccount(accountPrefix, accountPrefix+"pub")
-		config.SetBech32PrefixForValidator(accountPrefix+"valoper", accountPrefix+"valoperpub")
-		config.SetBech32PrefixForConsensusNode(accountPrefix+"valcons", accountPrefix+"valconspub")
-	}
-
 	// Create a temporary directory
 	tempDir := func() string {
 		dir, err := os.MkdirTemp("", "wasmd")
