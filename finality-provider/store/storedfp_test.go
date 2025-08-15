4fcf8adcb4f0cabeb6b7eb4662a2291a444358b2
liam.lai <liam.lai@us>
2025-08-15 13:24:09 -0700

    liam publish

---
+++ finality-provider/store/storedfp_test.go
@@ -0,0 +1,58 @@
+package store_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/babylonlabs-io/finality-provider/finality-provider/proto"
+	"github.com/babylonlabs-io/finality-provider/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestShouldStart(t *testing.T) {
+	t.Parallel()
+	tcs := []struct {
+		name           string
+		currFpStatus   proto.FinalityProviderStatus
+		expShouldStart bool
+	}{
+		{
+			"Created: Should NOT start",
+			proto.FinalityProviderStatus_CREATED,
+			false,
+		},
+		{
+			"Slashed: Should NOT start",
+			proto.FinalityProviderStatus_SLASHED,
+			false,
+		},
+		{
+			"Inactive: Should start",
+			proto.FinalityProviderStatus_INACTIVE,
+			true,
+		},
+		{
+			"Registered: Should start",
+			proto.FinalityProviderStatus_REGISTERED,
+			true,
+		},
+		{
+			"Active: Should start",
+			proto.FinalityProviderStatus_ACTIVE,
+			true,
+		},
+	}
+
+	r := rand.New(rand.NewSource(10))
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			fp := testutil.GenRandomFinalityProvider(r, t)
+			fp.Status = tc.currFpStatus
+
+			shouldStart := fp.ShouldStart()
+			require.Equal(t, tc.expShouldStart, shouldStart)
+		})
+	}
+}
