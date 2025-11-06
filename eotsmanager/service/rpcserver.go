bb3154a9b1fcd734e65e6305167fecd958a1b9fa
Lazar <lukic.lazar@yahoo.com>
2025-11-06 16:13:38 +0100

    lint fix

--- eotsmanager/service/rpcserver.go
+++ eotsmanager/service/rpcserver.go
@@ -94,7 +94,7 @@ func (r *rpcServer) SignEOTS(_ context.Context, req *proto.SignEOTSRequest) (
 func (r *rpcServer) UnsafeSignEOTS(_ context.Context, req *proto.SignEOTSRequest) (
 	*proto.SignEOTSResponse, error) {
 	if r.cfg.DisableUnsafeEndpoints {
-		return nil, status.Error(codes.PermissionDenied,
+		return nil, status.Error(codes.PermissionDenied, //nolint:wrapcheck
 			"UnsafeSignEOTS endpoint is disabled in configuration for security reasons")
 	}
 
